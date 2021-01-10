2d676e5293aee44febd67b6694eb0af66eacab16
sam <sam@freighttrust.com>
2021-01-09 17:27:26 -0800

    fix(conformance): ensure code conformance and lint-passing

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  singleQuote: false,
+  trailingComma: "es5",
+  proseWrap: "never",
+  endOfLine: "lf",
+};
