e91ceebbb7a38b41b654c79ca676ffc598b20c17
sam bacha <sam@freighttrust.com>
2021-03-12 03:42:17 -0800

    fix(manifold): exports

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,3 +1,12 @@
+// @file Manifold SDK
+// @summary TypedSDK
+// @exports manifold
+// @version latest
+// @license MIT
+
+"use strict";
+
 import * as manifold from "./manifold";
 export { manifold };
 export * from "./manifold";
+//@manifold
